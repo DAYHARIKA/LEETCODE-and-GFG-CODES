0f95a5fdc2b0cbd1142cf76f436d599a6686caae
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-08-31 12:56:56 +0530

    Time: 7 ms (53.02%), Space: 14.6 MB (52.8%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,22 +16,51 @@ public:
 
 class Solution {
 public:
+   void store_dummynodes(Node* head){
+      
+       Node* temp=head;
+       while(temp != NULL){
+         
+          Node* newnode=new Node(temp->val);
+          newnode->next=temp->next;
+          temp->next=newnode;
+          temp=temp->next->next;
+
+       }
+
+   }
+   void connect_randompointers(Node* head){
+        
+       Node* temp=head;
+       while(temp != NULL){
+
+           Node* newnode=temp->next;
+           if(temp->random != NULL){
+            newnode->random=temp->random->next;
+           }
+           temp=temp->next->next;
+
+       }
+   }
+   Node* connect_nextpointers(Node* head){
+
+       Node* dummy=new Node(-1);
+       Node* temp1=dummy;
+       Node* temp2=head;
+       while(temp2 != NULL){
+
+          temp1->next=temp2->next;
+          temp2->next=temp1->next->next;
+          temp1=temp1->next;
+          temp2=temp2->next;
+       }
+       return dummy->next;
+   }
     Node* copyRandomList(Node* head) {
         
-        unordered_map<Node*,Node*> mp;
-        Node* temp=head;
-        while(temp != NULL){
-            Node* newnode=new Node(temp->val);
-            mp[temp]=newnode;
-            temp=temp->next;
-        }
-        temp=head;
-        while(temp != NULL){
-            Node* newnode=mp[temp];
-            newnode->next=mp[temp->next];
-            newnode->random=mp[temp->random];
-            temp=temp->next;
-        }
-        return mp[head];
+        store_dummynodes(head);
+        connect_randompointers(head);
+        return connect_nextpointers(head);
+        
     }
 };
