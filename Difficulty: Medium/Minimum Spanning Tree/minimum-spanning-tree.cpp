21aa5fdc9abe7fde5c49126c7add9f06e6f9a264
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-09-10 21:38:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -3,6 +3,43 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class dis{
+    public:
+    vector<int> parent,size;
+    dis(int v){
+        parent.resize(v,0);
+        size.resize(v);
+        for(int i=0;i<v;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    
+    int ultimate_parent(int node){
+        if(node == parent[node]){
+            return node;
+        }else{
+            int p=ultimate_parent(parent[node]);
+            parent[node]=p;
+            return p;
+        }
+    }
+    
+    void union_size(int u,int v){
+        
+        int ul_u=ultimate_parent(u);
+        int ul_v=ultimate_parent(v);
+        
+        if(size[ul_u] > size[ul_v]){
+            parent[ul_v]=ul_u;
+            size[ul_u] += size[ul_v];
+        }else{
+            parent[ul_u]=ul_v;
+            size[ul_v] += size[ul_u];
+        }
+    }
+};
 class Solution
 {
 	public:
@@ -10,29 +47,35 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        vector<int> vis(V,0);
-        //{dis,node}
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        int sum=0;
-        pq.push({0,0});
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-            int w=it.first;
-            int n=it.second;
-            if(vis[n]==1)continue;
-            vis[n]=1;
-            sum += w;
-            for(auto it:adj[n]){
-                int n=it[0];
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i=0;i<V;i++){
+            
+            for(auto it:adj[i]){
+                int a=it[0];
                 int w=it[1];
-                if(!vis[n]){
-                   pq.push({w,n});
-                }
+                int n=i;
+                edges.push_back({w,{n,a}});
             }
         }
         
-        return sum;
+        //sort edges list
+        sort(edges.begin(),edges.end());
+        
+        dis d(V);
+        int wt=0;
+        for(auto it:edges){
+            
+            int w=it.first;
+            int n=it.second.first;
+            int a=it.second.second;
+            
+            if(d.ultimate_parent(n) != d.ultimate_parent(a)){
+                wt += w;
+                d.union_size(n,a);
+            }
+        }
+        
+        return wt;
     }
 };
 
