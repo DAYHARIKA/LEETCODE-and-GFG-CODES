e17ef865a2a613d1cbb0d0f2e18db071715b65b1
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-10-09 19:26:23 +0530

    Time: 0 ms (100%), Space: 8.9 MB (57.75%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,17 +4,21 @@ public:
         int n=matrix.size(),j;
         for(int i=0;i<matrix.size();i++){
             for( j=0;j<i;j++){
-               //swap(matrix[i][j],matrix[j][i]);
-               int temp=matrix[i][j];
-               matrix[i][j]=matrix[j][i];
-               matrix[j][i]=temp;
+               swap(matrix[i][j],matrix[j][i]);
+            //    int temp=matrix[i][j];
+            //    matrix[i][j]=matrix[j][i];
+            //    matrix[j][i]=temp;
             } 
         }
-        for(int i=0;i<matrix.size();i++){
-            for( j=0;j<matrix[i].size()/2;j++){
-                  swap(matrix[i][j],matrix[i][n-j-1]);
-            }
-        }
+        for(int i=0;i<n;i++)
+        reverse(matrix[i].begin(),matrix[i].end());
+        // for(int i=0;i<matrix.size();i++){
+        //     for( j=0;j<matrix[i].size()/2;j++){
+        //           swap(matrix[i][j],matrix[i][n-j-1]);
+        //     }
+        // }
+
+
         
         // for(int i=0;i<n;i++){
         //     for(int j=0;j<matrix[i].size();j++){
