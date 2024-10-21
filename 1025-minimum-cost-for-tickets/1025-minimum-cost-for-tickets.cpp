34f04aac69047e7139bf4108c6675e86b11ea123
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-10-21 21:15:43 +0530

    Time: 0 ms (100%), Space: 11.8 MB (85.76%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>& days, vector<int>& costs,int ind,vector<int>& dp){
-
-        if(ind >= days.size()){
-            return 0;
-        }
-        if(dp[ind] != -1) return dp[ind];
-        int op1=costs[0]+solve(days,costs,ind+1,dp);
+    int solve(vector<int>& days, vector<int>& costs,int n,vector<int>& dp){    
+      dp[n]=0;
+      for(int k=n-1;k>=0;k--){
+        int op1=costs[0]+dp[k+1];
         int i;
-        for(i=ind;i<days.size() && days[i]<days[ind]+7;i++);
-        int op2=costs[1]+solve(days,costs,i,dp);
+        for(i=k;i<days.size() && days[i]<days[k]+7;i++);
+        int op2=costs[1]+dp[i];
 
-        for(i=ind;i<days.size() && days[i]<days[ind]+30;i++);
-        int op3=costs[2]+solve(days,costs,i,dp);
+        for(i=k;i<days.size() && days[i]<days[k]+30;i++);
+        int op3=costs[2]+dp[i];
 
-        return dp[ind]=min(op1,min(op2,op3));
+         dp[k]=min(op1,min(op2,op3));
+      }
+      return dp[0];
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
           
          int n=days.size();
-         vector<int> dp(n+1,-1);
-         int ans=solve(days,costs,0,dp);
+         vector<int> dp(n+1,INT_MAX);
+         int ans=solve(days,costs,n,dp);
          return ans;
     }
 };
