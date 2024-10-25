b16773bbe6fa3e0e6f8e3c82b556f0e97449a049
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-10-25 21:18:04 +0530

    Time: 7 ms (96.37%), Space: 27.1 MB (33.81%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -6,14 +6,15 @@ public:
         int maxi=0;
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int> next(m+1,0);
         vector<int> curr(m+1,0);
         for(int i=n-1;i>=0;i--){
+            int prev=0;
          for(int j=m-1;j>=0;j--){
-
+            
+          int temp=curr[j];
           int adj=curr[j+1];
-          int dia=next[j+1];
-          int bot=next[j];
+          int dia=prev;
+          int bot=curr[j];
 
            if(matrix[i][j] == '1'){
             curr[j]=1+min(adj,min(dia,bot));
@@ -21,8 +22,8 @@ public:
            }else{
              curr[j]=0;
            }
-        }
-        next=curr;                                                      
+           prev=temp;
+        }  
        }
         return maxi*maxi;
     }
