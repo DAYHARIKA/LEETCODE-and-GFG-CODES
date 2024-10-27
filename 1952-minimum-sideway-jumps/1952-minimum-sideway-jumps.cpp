3b2601e8445b7bb03c810e1686d3df390fe1992b
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-10-27 15:07:23 +0530

    Time: 55 ms (46.56%), Space: 190.6 MB (89.68%) - LeetHub

--- 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
@@ -4,26 +4,28 @@ public:
     int minSideJumps(vector<int>& obstacles) {
         
         int n=obstacles.size();
-        vector<vector<int>> dp(4,vector<int>(n,INT_MAX));
-        dp[0][n-1]=0;
-        dp[1][n-1]=0;
-        dp[2][n-1]=0;
-        dp[3][n-1]=0;
+        vector<int> curr(4,INT_MAX);
+        vector<int> next(4,INT_MAX);
+        next[0]=0;
+        next[1]=0;
+        next[2]=0;
+        next[3]=0;
         for(int curpos=n-2;curpos>=0;curpos--){
             for(int curlane=1;curlane<=3;curlane++){
 
                 if(obstacles[curpos+1] != curlane){
-                    dp[curlane][curpos]=dp[curlane][curpos+1];
+                    curr[curlane]=next[curlane];
                 }else{
                     int ans=INT_MAX;
                     for(int i=1;i<=3;i++){
                         if(curlane != i && obstacles[curpos] != i)
-                        ans=min(ans,1+dp[i][curpos+1]);
+                        ans=min(ans,1+next[i]);
                     }
-                    dp[curlane][curpos]=ans;
+                    curr[curlane]=ans;
                 }
             }
+            next=curr;
         }
-        return min(dp[1][0]+1,min(dp[2][0],dp[3][0]+1));
+        return min(next[1]+1,min(next[2],next[3]+1));
     }
 };
