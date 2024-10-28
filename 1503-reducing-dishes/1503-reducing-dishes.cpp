699957cf9d608987098d9b908005f3c8c2036e37
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-10-28 20:40:44 +0530

    Time: 22 ms (30.51%), Space: 23.7 MB (8.18%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(vector<int>& satisfaction,int n,int index,int time,vector<vector<int>>& dp){
+    /*int solve(vector<int>& satisfaction,int n,int index,int time,vector<vector<int>>& dp){
 
         if(index == n){
             return 0;
@@ -9,11 +9,20 @@ public:
         int include=(time+1)*(satisfaction[index])+solve(satisfaction,n,index+1,time+1,dp);
         int exclude=solve(satisfaction,n,index+1,time,dp);
         return dp[index][time]=max(include,exclude);
-    }
+    }*/
     int maxSatisfaction(vector<int>& satisfaction) {
         int n=satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return solve(satisfaction,n,0,0,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<n;j++){
+
+                int include=((j+1)*(satisfaction[i]))+dp[i+1][j+1];
+                int exclude=dp[i+1][j];
+                dp[i][j]=max(include,exclude); 
+
+            }
+        }
+        return dp[0][0];
     }
 };
