00f1a8454db30b82c0bd19ec64a20b9aa2a1afac
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-10-29 22:19:40 +0530

    Time: 163 ms (37.15%), Space: 13.4 MB (40.32%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int n,int curr,int prev,vector<vector<int>>& dp){
+    /*int solve(vector<int>& nums,int n,int curr,int prev,vector<vector<int>>& dp){
 
         if(curr == n)return 0;
         if(dp[curr][prev+1] != -1)return dp[curr][prev+1];
@@ -10,10 +10,25 @@ public:
 
         int exclude=0+solve(nums,n,curr+1,prev,dp);
         return dp[curr][prev+1]=max(include,exclude);
-    }
+    }*/
     int lengthOfLIS(vector<int>& nums) {
+
         int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solve(nums,n,0,-1,dp);
+        vector<int> cur(n+1,0);
+        vector<int> next(n+1,0);
+        for(int curr=n-1;curr>=0;curr--){
+            for(int prev=curr-1;prev>=-1;prev--){
+
+                int include=0;
+                if(prev==-1 || nums[curr] > nums[prev])
+                include=1+next[curr+1];
+
+                int exclude=0+next[prev+1];
+                cur[prev+1]=max(include,exclude); 
+
+            }
+            next=cur;
+        }
+        return next[0];
     }
 };
