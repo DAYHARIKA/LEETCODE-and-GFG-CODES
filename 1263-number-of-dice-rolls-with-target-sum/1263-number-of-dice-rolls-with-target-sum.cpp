890c25f7b000289b8b7c423b50951977c450e95d
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-03 12:58:01 +0530

    Time: 14 ms (68.84%), Space: 8.1 MB (83.58%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
@@ -3,19 +3,21 @@ public:
     int mod=1e9+7;
     int numRollsToTarget(int n, int k, int target) {
 
-        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
-        dp[0][0]=1;
+        vector<int> prev(target+1,0);
+        vector<int> curr(target+1,0);
+        prev[0]=1;
         for(int dices=1;dices<=n;dices++){
             for(int targetsum=1;targetsum<=target;targetsum++){
 
                 int ans=0;
                 for(int i=1;i<=k;i++){
                     if(targetsum-i >= 0)
-                    ans =(ans+ dp[dices-1][targetsum-i])%mod;
+                    ans =(ans+ prev[targetsum-i])%mod;
                 }
-                dp[dices][targetsum]=ans;
+                curr[targetsum]=ans;
             }
+            prev=curr;
         }
-        return dp[n][target];
+        return prev[target];
     }
 };
