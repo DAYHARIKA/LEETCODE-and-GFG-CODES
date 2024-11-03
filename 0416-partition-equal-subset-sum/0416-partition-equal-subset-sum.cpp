bebf4deced2f8f6ec45e34da796252f1d937dde4
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-03 14:24:07 +0530

    Time: 346 ms (11.69%), Space: 16.5 MB (62.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums,int n,int ind,int target,vector<vector<int>>& dp){
-
-        if(ind >= n)return false;
-        if(target < 0)return false;
-        if(target == 0)return  true;
-        if(dp[ind][target] != -1)return dp[ind][target];
-        bool inclu=solve(nums,n,ind+1,target-nums[ind],dp);
-        bool exclu=solve(nums,n,ind+1,target-0,dp);
-
-        return dp[ind][target]=(inclu || exclu);
-    }
+    
     bool canPartition(vector<int>& nums) {
-        
+        int n=nums.size();
         int totalsum=0;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             totalsum += nums[i];
         }
         if(totalsum%2 == 1)return false;
         
-        vector<vector<int>> dp(nums.size()+1,vector<int>((totalsum/2)+1,-1));
-        return solve(nums,nums.size(),0,totalsum/2,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>((totalsum/2)+1,false));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+        }
+        for(int ind=n-1;ind>=0;ind--){
+            for(int target=1;target<=(totalsum/2);target++){
+                bool inclu=false;
+
+                if(target-nums[ind]>=0)
+                inclu=dp[ind+1][target-nums[ind]];
+
+                bool exclu=dp[ind+1][target-0];
+
+               dp[ind][target]=(inclu || exclu);
+            }
+        }
+        return dp[0][totalsum/2];
     }
 };
