c78993a3cea676cb284349c58f3ca7084dd08a48
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-04 22:05:25 +0530

    Time: 95 ms (33.33%), Space: 123.2 MB (58.52%) - LeetHub

--- 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
@@ -7,6 +7,7 @@ public:
         }
 
         if(dp[ind][swapped] != -1)return dp[ind][swapped];
+
         int ans=INT_MAX;
         int prev1 = (ind == 0) ? -1 : nums1[ind - 1]; 
         int prev2 = (ind == 0) ? -1 : nums2[ind - 1];
@@ -27,8 +28,28 @@ public:
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         
         int n=nums1.size();
-        bool swapped=false;
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(nums1,nums2,0,swapped,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int swapped=1;swapped>=0;swapped--){
+
+                int ans=INT_MAX;
+                int prev1 = (ind == 0) ? -1 : nums1[ind - 1]; 
+                int prev2 = (ind == 0) ? -1 : nums2[ind - 1];
+
+                if(swapped)swap(prev1,prev2);
+
+                //noswap
+                if(nums1[ind]>prev1 && nums2[ind] > prev2)
+                ans=dp[ind+1][0];
+
+                //swap
+                if(nums1[ind]>prev2 && nums2[ind] > prev1)
+                ans=min(ans,1+dp[ind+1][1]);
+
+                dp[ind][swapped]=ans;
+            }
+        }
+         return dp[0][0];
     }
 };
