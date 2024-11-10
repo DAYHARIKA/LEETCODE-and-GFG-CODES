9e8c50e0868fab05ab3a85cd513f6e7f6d3c78fc
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-10 12:37:01 +0530

    Time: 35 ms (91.08%), Space: 9.6 MB (68.5%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int solve(int s,int e,vector<vector<int>> &dp){
-        
-        if(s >= e)return 0;
-        if(dp[s][e] != -1)return dp[s][e];
-        int mini=INT_MAX;
-        for(int i=s;i<=e;i++){
-            mini=min(mini,i+max(solve(s,i-1,dp),solve(i+1,e,dp)));
-        }
-        return dp[s][e]=mini;
-    }
+    
     int getMoneyAmount(int n) {
         
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        int ans=solve(1,n,dp);
-        return ans;
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+
+        for(int s=n;s>=1;s--){
+            for(int e=s;e<=n;e++){
+
+                if(s>=e)continue;
+                else{
+                    int mini=INT_MAX;
+                    for(int i=s;i<=e;i++){
+                        mini=min(mini,i+max(dp[s][i-1],dp[i+1][e]));
+                    }
+                    dp[s][e]=mini;
+                }
+            }
+        }
+        
+        return dp[1][n];
     }
 };
