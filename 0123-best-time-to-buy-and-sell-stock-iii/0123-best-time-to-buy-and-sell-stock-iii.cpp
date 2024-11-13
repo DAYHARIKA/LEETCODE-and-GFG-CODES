07f53736d8766e3857987c1b1d50d4741f13a079
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-13 22:02:28 +0530

    Time: 382 ms (45.05%), Space: 209.8 MB (51.18%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>& prices,int n,int index,int buy,int limit,vector<vector<vector<int>>> &dp){
-
-        if(index >= n)return 0;
-        if(limit == 0)return 0;
-        int profit=0;
-        int skip=0;
-        int maxi=INT_MIN;
-        if(dp[index][buy][limit] != -1)return dp[index][buy][limit];
-        //buying----
-        if(buy){
-           profit += (-prices[index])+solve(prices,n,index+1,0,limit,dp);
-           skip += 0+solve(prices,n,index+1,1,limit,dp);
-           maxi=max(profit,skip);
-        }else{
-           profit += (prices[index])+solve(prices,n,index+1,1,limit-1,dp);
-           skip += 0+solve(prices,n,index+1,0,limit,dp); 
-           maxi=max(profit,skip);
-        }
-        return dp[index][buy][limit]=maxi;
-    }
     int maxProfit(vector<int>& prices) {
         
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        int ans=solve(prices,n,0,1,2,dp);
-        return ans;
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int index=n-1;index>=0;index--){
+            for(int buy=1;buy>=0;buy--){
+                for(int limit=1;limit<3;limit++){
+                    int profit=0;
+                    int skip=0;
+                    if(buy){
+                    profit += (-prices[index])+dp[index+1][0][limit];
+                    skip += 0+dp[index+1][1][limit];
+                    }else{
+                    profit += (prices[index])+dp[index+1][1][limit-1];
+                    skip += 0+dp[index+1][0][limit]; 
+                    }
+                    dp[index][buy][limit]=max(profit,skip);
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
