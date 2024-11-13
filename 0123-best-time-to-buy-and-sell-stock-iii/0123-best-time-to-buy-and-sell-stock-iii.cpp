47a6cd9e566b7f7913a9ec55a635070af5fe344c
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-13 22:06:41 +0530

    Time: 88 ms (61.22%), Space: 78.2 MB (86.04%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,23 +3,25 @@ public:
     int maxProfit(vector<int>& prices) {
         
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> curr(2,vector<int>(3,0));
+         vector<vector<int>> prev(2,vector<int>(3,0));
         for(int index=n-1;index>=0;index--){
             for(int buy=1;buy>=0;buy--){
                 for(int limit=1;limit<3;limit++){
                     int profit=0;
                     int skip=0;
                     if(buy){
-                    profit += (-prices[index])+dp[index+1][0][limit];
-                    skip += 0+dp[index+1][1][limit];
+                    profit += (-prices[index])+prev[0][limit];
+                    skip += 0+prev[1][limit];
                     }else{
-                    profit += (prices[index])+dp[index+1][1][limit-1];
-                    skip += 0+dp[index+1][0][limit]; 
+                    profit += (prices[index])+prev[1][limit-1];
+                    skip += 0+prev[0][limit]; 
                     }
-                    dp[index][buy][limit]=max(profit,skip);
+                    curr[buy][limit]=max(profit,skip);
                 }
+                prev=curr;
             }
         }
-        return dp[0][1][2];
+        return prev[1][2];
     }
 };
