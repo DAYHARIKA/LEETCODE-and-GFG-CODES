377458a6975c8e8114494f499b5a969b6569c9a2
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-18 13:58:32 +0530

    Time: 0 ms (100%), Space: 13 MB (39.44%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    int solve(string &a, string &b, int i, int j, vector<vector<int>> &dp){
-
-        if(i == a.length()){
-            return b.length()-j;
-        }
-        if(j == b.length()){
-            return a.length()-i;
-        }
-        if(dp[i][j] != -1)return dp[i][j];
-        int ans=0;
-        if(a[i] == b[j]){
-            return solve(a,b,i+1,j+1,dp);
-        }else{
-            int ins=1+solve(a,b,i,j+1,dp);
-            int del=1+solve(a,b,i+1,j,dp);
-            int rep=1+solve(a,b,i+1,j+1,dp);
-            ans=min(ins,min(del,rep));
-        }
-        return dp[i][j]=ans;
-    }
+    
     int minDistance(string word1, string word2) {
         
         int n1=word1.length();
         int n2=word2.length();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return solve(word1,word2,0,0,dp);
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+
+        for(int j=0;j<=n2;j++){
+            dp[n1][j]=n2-j;
+        }
+        for(int i=0;i<=n1;i++){
+            dp[i][n2]=n1-i;
+        }
+
+        for(int i=n1-1;i>=0;i--){
+            for(int j=n2-1;j>=0;j--){
+                int ans=0;
+                if(word1[i] == word2[j]){
+                    ans = dp[i+1][j+1];
+                }else{
+                    int ins=1+dp[i][j+1];
+                    int del=1+dp[i+1][j];
+                    int rep=1+dp[i+1][j+1];
+                    ans=min(ins,min(del,rep));
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
     }
 };
