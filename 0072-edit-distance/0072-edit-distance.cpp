d05926b61ee74357694ded357828e4fd0919adfb
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-18 14:13:52 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.56%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,29 +5,34 @@ public:
         
         int n1=word1.length();
         int n2=word2.length();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
-
-        for(int j=0;j<=n2;j++){
-            dp[n1][j]=n2-j;
+        if(n1 == 0){
+            return n2;
         }
-        for(int i=0;i<=n1;i++){
-            dp[i][n2]=n1-i;
+        if(n2 == 0){
+            return n1;
+        }
+        vector<int> curr(n2+1,0);
+        vector<int> next(n2+1,0);
+        for(int j=0;j<=n2;j++){
+            next[j]=n2-j;
         }
 
         for(int i=n1-1;i>=0;i--){
             for(int j=n2-1;j>=0;j--){
+                curr[n2]=n1-i;
                 int ans=0;
                 if(word1[i] == word2[j]){
-                    ans = dp[i+1][j+1];
+                    ans = next[j+1];
                 }else{
-                    int ins=1+dp[i][j+1];
-                    int del=1+dp[i+1][j];
-                    int rep=1+dp[i+1][j+1];
+                    int ins=1+curr[j+1];
+                    int del=1+next[j];
+                    int rep=1+next[j+1];
                     ans=min(ins,min(del,rep));
                 }
-                dp[i][j]=ans;
+                curr[j]=ans;
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
