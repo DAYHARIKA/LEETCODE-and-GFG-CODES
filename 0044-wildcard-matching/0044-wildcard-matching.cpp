cdf647cf9b5865e0f5c4bea67827ebe9ca14666c
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2024-11-18 16:44:32 +0530

    Time: 16 ms (92.53%), Space: 10.8 MB (93.69%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -4,8 +4,9 @@ public:
     bool isMatch(string s, string p) {
          int n1=s.length();
          int n2=p.length();
-         vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
-         dp[0][0]=true;
+         vector<int> curr(n2+1,0);
+         vector<int> prev(n2+1,0);
+         prev[0]=true;
          for(int j=1;j<=n2;j++){
              int flag=true;
             for(int k=1;k<=j;k++){
@@ -14,19 +15,20 @@ public:
                  break;
               }
            }
-           dp[0][j]=flag;
+           prev[j]=flag;
          }
          for(int i=1;i<=n1;i++){
             for(int j=1;j<=n2;j++){
                 if(s[i-1] == p[j-1] || p[j-1] == '?'){
-                    dp[i][j]=dp[i-1][j-1];
+                   curr[j]=prev[j-1];
                 }else if(p[j-1] == '*'){
-                    dp[i][j]= (dp[i][j-1] || dp[i-1][j]);
+                    curr[j]= (curr[j-1] || prev[j]);
                 }else{
-                    dp[i][j]=  false;
+                    curr[j]=  false;
                 }
             }
+            prev=curr;
          }
-         return dp[n1][n2];
+         return prev[n2];
     }
 };
