8c6d100414aca34c0441bc8decb09c28d99b2ebc
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-01-16 14:15:09 +0530

    Time: 0 ms (100%), Space: 29.5 MB (69.1%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    void bruteforce(vector<int>& nums, int k ,int n){
+    // void bruteforce(vector<int>& nums, int k ,int n){
 
-        k=k%n; 
-        vector<int> temp(k);
-        for(int i=0;i<k;i++){
-            temp[i]=nums[n-k+i];
+    //     k=k%n; 
+    //     vector<int> temp(k);
+    //     for(int i=0;i<k;i++){
+    //         temp[i]=nums[n-k+i];
+    //     }
+    //     for(int i=n-1;i>=k;i--){
+    //         nums[i]=nums[i-k];
+    //     }
+    //     for(int i=0;i<k;i++){
+    //         nums[i]=temp[i];
+    //     }
+
+    // }
+    void reverse(vector<int> &nums,int i,int j){
+
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
         }
-        for(int i=n-1;i>=k;i--){
-            nums[i]=nums[i-k];
-        }
-        for(int i=0;i<k;i++){
-            nums[i]=temp[i];
-        }
+    }
+    void optimized(vector<int>& nums, int k ,int n){
+         k=k%n;
+         reverse(nums,0,n-k-1);
+         reverse(nums,n-k,n-1);
+         reverse(nums,0,n-1);
 
     }
     void rotate(vector<int>& nums, int k) {
          int n=nums.size();
-         bruteforce(nums,k,n);
+         //bruteforce(nums,k,n);
+         optimized(nums,k,n);
     }
 };
