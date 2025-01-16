fda9d8b4811f793355c001c1a3a55c9ebab1e9e6
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-01-16 15:24:05 +0530

    Time: 4 ms (14.92%), Space: 24.3 MB (7.75%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+         int n=nums.size();
+         vector<int> ans(n,0);
+         int k=0;
+         for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                ans[k]=nums[i];
+                k++;
+            }
+         }
+         for(int i=0;i<n;i++){
+            nums[i]=ans[i];
+         }
+    }
+};
