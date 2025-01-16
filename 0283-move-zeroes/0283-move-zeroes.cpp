b7381ea1a1e2bec3f0acb05cd10c0d8d93b8057d
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-01-16 15:47:29 +0530

    Time: 0 ms (100%), Space: 24 MB (15.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
+    void bruteforce(vector<int>& nums){
          int n=nums.size();
          vector<int> ans(n,0);
          int k=0;
@@ -12,6 +12,16 @@ public:
          }
          for(int i=0;i<n;i++){
             nums[i]=ans[i];
+         }
+    }
+    void moveZeroes(vector<int>& nums) {
+         int i=0;
+         for(int j=0;j<nums.size();j++){
+
+            if(nums[j] != 0){
+                swap(nums[j],nums[i]);
+                i++;
+            }
          }
     }
 };
