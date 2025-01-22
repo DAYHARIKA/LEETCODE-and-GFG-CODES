0956405b253d5dbb559beeb2664a7e2d1d6c9e5b
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-01-22 22:18:38 +0530

    Time: 0 ms (100%), Space: 97.4 MB (24.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,7 +3,6 @@ public:
     int maxProfit(vector<int>& prices) {
         
         int n=prices.size();
-
         int mini=prices[0];
         int profit=0;
         for(int i=1;i<n;i++){
@@ -11,9 +10,7 @@ public:
             int diff=prices[i]-mini;
             profit=max(profit,diff);
             mini=min(mini,prices[i]);
-
         }
         return profit;
-        
     }
 };
