8346b959ddf2f00fc092f84f9da0beb79bdf22ce
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-01-29 22:38:50 +0530

    Time: 1 ms (23.96%), Space: 19.7 MB (97.64%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,20 +3,33 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         
         vector<int> ans;
-        unordered_map<int,int> mp;
         int n=nums.size();
 
+        int ele1=INT_MIN,ele2=INT_MIN,cnt1=0,cnt2=0;
+
         for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
 
-        for(auto i:mp){
-
-            if(i.second > (n/3)){
-                ans.push_back(i.first);
+            if(cnt1 == 0 && ele2 != nums[i]){
+                cnt1=1;
+                ele1=nums[i];
+            }else if(cnt2 == 0 && ele1 != nums[i]){
+                cnt2=1;
+                ele2=nums[i];
+            }else if(ele1 == nums[i])cnt1++;
+            else if(ele2 == nums[i])cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-
+        cnt1=0,cnt2=0;
+        for(int i=0;i<n;i++){
+            if(ele1 == nums[i])cnt1++;
+            if(ele2 == nums[i])cnt2++;
+        }
+        int mini=(int)(n/3)+1;
+        if(cnt1 >= mini)ans.push_back(ele1);
+        if(cnt2 >= mini)ans.push_back(ele2);
         return ans;
     }
 };
